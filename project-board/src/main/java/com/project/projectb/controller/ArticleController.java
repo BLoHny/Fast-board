7c0bc89d8fe7f2001ec5dbf33824648fdff922ef
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-02-19 19:58:43 +0900

    ss

--- project-board/src/main/java/com/project/projectb/controller/ArticleController.java
+++ project-board/src/main/java/com/project/projectb/controller/ArticleController.java
@@ -1,8 +1,10 @@
 package com.project.projectb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -11,9 +13,10 @@ import java.util.List;
 @Controller
 public class ArticleController {
 
-    @RequestMapping("/index")
-    public String articles() {
-        return "index";
+    @GetMapping
+    public String articles(ModelMap map) {
+        map.addAttribute("articles", List.of());
+        return "articles/index";
     }
 
 }
